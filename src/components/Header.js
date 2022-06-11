8c9c4496e8f4f9b06b31d5c4acdc6897266f8caa
naiiiden <na4ko359@gmail.com>
2022-06-11 07:15:13 +0300

    header component increase link font-size on tablet+

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,14 @@
+import { useState } from "react";
 import Logo from "../images/square.svg";
 
 const Header = () => {
+    const [hover, setHover] = useState(false);
+
     return (
         <header>
             <img src={Logo} alt="Red Square's logo" className="header--logo"/>
             <nav>
-                <a href="#" className="nav--link">Work <span>&gt;</span></a>
+                <a href="#" className="nav--link" onMouseOver={() => (setHover(false))} onMouseLeave={() => (setHover(true) )}>Work <span>&gt;</span></a>
             </nav>
         </header>
     )
