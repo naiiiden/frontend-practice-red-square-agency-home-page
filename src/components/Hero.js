facf356b336100d0249d831a330906a3a30ea71f
naiiiden <na4ko359@gmail.com>
2022-06-12 19:59:20 +0300

    hero component cleanup & delete unnecessary files

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,17 +1,3 @@
-import img1 from "../images/1.jpg";
-import img2 from "../images/2.jpg";
-import img3 from "../images/3.jpg";
-import img4 from "../images/4.jpg";
-import img5 from "../images/5.jpg";
-import img6 from "../images/6.jpg";
-import img7 from "../images/7.jpg";
-import img8 from "../images/8.jpg";
-import img9 from "../images/9.jpg";
-import img10 from "../images/10.jpg";
-import img11 from "../images/11.jpg";
-import img12 from "../images/12.jpg";
-
-
 const Hero = () => {
     return (
         <main>
@@ -25,19 +11,6 @@ const Hero = () => {
             </h1>
             <div className="hero--slideshow">
                 <a href="https://www.redsquareagency.com/work">
-                    {/* <img src={img4} alt="" className="img img4"/>
-                    <img src={img12} alt="" className="img img12"/>
-                    <img src={img10} alt="" className="img img10"/>
-                    <img src={img9} alt="" className="img img9"/>
-                    <img src={img11} alt="" className="img img11"/>
-                    <img src={img2} alt="" className="img img2"/>
-                    <img src={img1} alt="" className="img img1"/>
-                    <img src={img6} alt="" className="img img6"/>
-                    <img src={img3} alt="" className="img img3"/>
-                    <img src={img7} alt="" className="img img7"/>
-                    <img src={img5} alt="" className="img img5"/>
-                    <img src={img8} alt="" className="img img8"/> */}
-
                 </a>
             </div>
         </main>
