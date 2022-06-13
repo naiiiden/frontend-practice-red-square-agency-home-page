5c14637297540c49b05e8ff8345e5596454190ce
naiiiden <na4ko359@gmail.com>
2022-06-13 07:03:20 +0300

    idea section component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import "./style/general.css";
 import Header from "./components/Header";
 import Hero from "./components/Hero";
+import IdeaSection from "./components/IdeaSection";
 
 const App = () => {
   return (
     <div>
       <Header/>
       <Hero/>
-      <h1>TEST</h1>
+      <IdeaSection/>
     </div>
   )
 }
---
+++ src/components/IdeaSection.js
@@ -0,0 +1,10 @@
+const IdeaSection = () => {
+    return (
+        <section className="idea--section">
+            <h2 className="idea--section--h2">We deal in ideas, design and media that are category defying. When the world Millis, you’ve got to Vanilli.</h2>
+            <p className="idea--section--p">Red Square believes in rigorous development of brand strategy and whip-smart execution. We make all sorts of things. Things that move markets, compel audiences and sell product. We help great brands create what comes next.</p>
+        </section>
+    )
+}
+
+export default IdeaSection;
