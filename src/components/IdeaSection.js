064f818f5e2dc604843281c3083b6d8212328c46
naiiiden <na4ko359@gmail.com>
2022-06-14 12:09:23 +0300

    import wow.js/animate.css & test

--- src/components/IdeaSection.js
+++ src/components/IdeaSection.js
@@ -1,7 +1,12 @@
+import "animate.css";
+import WOW from "wow.js";
+
 const IdeaSection = () => {
+    new WOW().init();
+
     return (
         <section className="idea--section">
-            <h2 className="idea--section--h2">We deal in ideas, design and media that are category defying. When the world Millis, you’ve got to Vanilli.</h2>
+            <h2 className="idea--section--h2 wow animate__animated animate__bounce">We deal in ideas, design and media that are category defying. When the world Millis, you’ve got to Vanilli.</h2>
             <p className="idea--section--p">Red Square believes in rigorous development of brand strategy and whip-smart execution. We make all sorts of things. Things that move markets, compel audiences and sell product. We help great brands create what comes next.</p>
         </section>
     )
