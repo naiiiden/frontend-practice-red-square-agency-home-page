31bd3289e6c900f1d31861fba03ada206993b549
naiiiden <na4ko359@gmail.com>
2022-06-14 13:37:44 +0300

    hero component add conditional render based on window innerWidth(render slideshow without slideInUp animation on mobile)

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,8 +1,19 @@
 import "animate.css";
 import WOW from "wow.js";
+import { useEffect, useState } from "react";
 
 const Hero = () => {
-    new WOW().init();
+    new WOW({
+        live: false
+    }).init();
+
+    const [isDesktop, setIsDesktop] = useState(window.innerWidth > 1023);
+    const updateMedia = () => setIsDesktop(window.innerWidth > 1023);
+
+    useEffect(() => {
+        window.addEventListener("resize", updateMedia);
+        return () => window.removeEventListener("resize", updateMedia);
+    })
     
     return (
         <main>
@@ -14,10 +25,17 @@ const Hero = () => {
                 <span className="hero--heading--span span5">comes </span> 
                 <span className="hero--heading--span span6">next.</span>
             </h1>
+            {isDesktop ? (
             <div className="slideshow hero--slideshow wow animate__animated animate__slideInUp animate__delay-3s">
                 <a href="https://www.redsquareagency.com/work">
                 </a>
             </div>
+            ) : (
+                <div className="hero--slideshow no--animation">
+                <a href="https://www.redsquareagency.com/work">
+                </a>
+            </div>
+            )}
         </main>
     )
 }
