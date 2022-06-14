10757446dda33756f0bd4351ef5a95b91ea16419
naiiiden <na4ko359@gmail.com>
2022-06-14 12:18:10 +0300

    idea section add slideInUp animation on reveal

--- src/components/IdeaSection.js
+++ src/components/IdeaSection.js
@@ -6,8 +6,8 @@ const IdeaSection = () => {
 
     return (
         <section className="idea--section">
-            <h2 className="idea--section--h2 wow animate__animated animate__bounce">We deal in ideas, design and media that are category defying. When the world Millis, you’ve got to Vanilli.</h2>
-            <p className="idea--section--p">Red Square believes in rigorous development of brand strategy and whip-smart execution. We make all sorts of things. Things that move markets, compel audiences and sell product. We help great brands create what comes next.</p>
+            <h2 className="idea--section--h2 wow animate__animated animate__slideInUp">We deal in ideas, design and media that are category defying. When the world Millis, you’ve got to Vanilli.</h2>
+            <p className="idea--section--p wow animate__animated animate__slideInUp">Red Square believes in rigorous development of brand strategy and whip-smart execution. We make all sorts of things. Things that move markets, compel audiences and sell product. We help great brands create what comes next.</p>
         </section>
     )
 }
