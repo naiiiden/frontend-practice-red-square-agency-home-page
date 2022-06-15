3a309b74dab19e7fd8511b9468bcb74ef517e72c
naiiiden <na4ko359@gmail.com>
2022-06-15 07:02:34 +0300

    hero component heading increase top padding on all resolutions

--- src/components/IdeaSection.js
+++ src/components/IdeaSection.js
@@ -2,14 +2,12 @@ import "animate.css";
 import WOW from "wow.js";
 
 const IdeaSection = () => {
-    new WOW({
-        offset: 200,
-    }).init();
+    new WOW().init();
 
     return (
         <section className="idea--section">
-            <h2 className="idea--section--h2 wow animate__animated animate__slideInUp">We deal in ideas, design and media that are category defying. When the world Millis, you’ve got to Vanilli.</h2>
-            <p className="idea--section--p wow animate__animated animate__slideInUp">Red Square believes in rigorous development of brand strategy and whip-smart execution. We make all sorts of things. Things that move markets, compel audiences and sell product. We help great brands create what comes next.</p>
+            <h2 className="idea--section--h2 wow animate__animated animate__slideInUp data-wow-offset='200'">We deal in ideas, design and media that are category defying. When the world Millis, you’ve got to Vanilli.</h2>
+            <p className="idea--section--p wow animate__animated animate__slideInUp data-wow-offset='200'">Red Square believes in rigorous development of brand strategy and whip-smart execution. We make all sorts of things. Things that move markets, compel audiences and sell product. We help great brands create what comes next.</p>
         </section>
     )
 }
