ceaa72fcf8646beb641b87a3ec53e9c7920359e5
naiiiden <na4ko359@gmail.com>
2022-06-16 22:01:31 +0300

    clients component add animation reveal delay on clients list

--- src/components/Clients.js
+++ src/components/Clients.js
@@ -11,26 +11,26 @@ const Clients = () => {
                 <h3 className="clients--section--heading wow animate__animated animate__fadeIn">Select Clients</h3>
                 <div className="clients--ul--container">
                     <ul className="clients--ul">
-                        <li className="wow animate__animated animate__fadeInLeft">Cherokee Casinos</li>
-                        <li className="wow animate__animated animate__fadeInLeft">Flipboard</li>
-                        <li className="wow animate__animated animate__fadeInLeft">Foxwoods</li>
-                        <li className="wow animate__animated animate__fadeInLeft">Glanbia Nutritionals</li>
-                        <li className="wow animate__animated animate__fadeInLeft">Google</li>
-                        <li className="wow animate__animated animate__fadeInLeft">Hard Rock</li>
-                        <li className="wow animate__animated animate__fadeInLeft">Hibbett Sports</li>
-                        <li className="wow animate__animated animate__fadeInLeft">Hilton Worldwide</li>
-                        <li className="wow animate__animated animate__fadeInLeft">Jack Daniel's</li>
+                        <li className="wow animate__animated animate__fadeInLeft" data-wow-delay=".0100s">Cherokee Casinos</li>
+                        <li className="wow animate__animated animate__fadeInLeft" data-wow-delay=".0150s">Flipboard</li>
+                        <li className="wow animate__animated animate__fadeInLeft" data-wow-delay=".0200s">Foxwoods</li>
+                        <li className="wow animate__animated animate__fadeInLeft" data-wow-delay=".0250s">Glanbia Nutritionals</li>
+                        <li className="wow animate__animated animate__fadeInLeft" data-wow-delay=".0300s">Google</li>
+                        <li className="wow animate__animated animate__fadeInLeft" data-wow-delay=".0350s">Hard Rock</li>
+                        <li className="wow animate__animated animate__fadeInLeft" data-wow-delay=".0400s">Hibbett Sports</li>
+                        <li className="wow animate__animated animate__fadeInLeft" data-wow-delay=".0450s">Hilton Worldwide</li>
+                        <li className="wow animate__animated animate__fadeInLeft" data-wow-delay=".0500s">Jack Daniel's</li>
                     </ul>
                     <ul className="clients--ul">
-                        <li className="wow animate__animated animate__fadeInLeft">Nescafé</li>
-                        <li className="wow animate__animated animate__fadeInLeft">New York Pride</li>
-                        <li className="wow animate__animated animate__fadeInLeft">Patreon</li>
-                        <li className="wow animate__animated animate__fadeInLeft">Rivers Casino</li>
-                        <li className="wow animate__animated animate__fadeInLeft">Snickers</li>
-                        <li className="wow animate__animated animate__fadeInLeft">Southern Comfort</li>
-                        <li className="wow animate__animated animate__fadeInLeft">The University of Alabama</li>
-                        <li className="wow animate__animated animate__fadeInLeft">Twix</li>
-                        <li className="wow animate__animated animate__fadeInLeft">Wind Creek Hospitality</li>
+                        <li className="wow animate__animated animate__fadeInLeft" data-wow-delay=".0100s">Nescafé</li>
+                        <li className="wow animate__animated animate__fadeInLeft" data-wow-delay=".0150s">New York Pride</li>
+                        <li className="wow animate__animated animate__fadeInLeft" data-wow-delay=".0200s">Patreon</li>
+                        <li className="wow animate__animated animate__fadeInLeft" data-wow-delay=".0250s">Rivers Casino</li>
+                        <li className="wow animate__animated animate__fadeInLeft" data-wow-delay=".0300s">Snickers</li>
+                        <li className="wow animate__animated animate__fadeInLeft" data-wow-delay=".0350s">Southern Comfort</li>
+                        <li className="wow animate__animated animate__fadeInLeft" data-wow-delay=".0400s">The University of Alabama</li>
+                        <li className="wow animate__animated animate__fadeInLeft" data-wow-delay=".0450s">Twix</li>
+                        <li className="wow animate__animated animate__fadeInLeft" data-wow-delay=".0500s">Wind Creek Hospitality</li>
                     </ul>
                 </div>
             </section>
