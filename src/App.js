dd57872b449d854b19b9f2d9131c1d0f994e3ef9
naiiiden <na4ko359@gmail.com>
2022-06-17 18:12:31 +0300

    footer component

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Header from "./components/Header";
 import Hero from "./components/Hero";
 import IdeaSection from "./components/IdeaSection";
 import Clients from "./components/Clients";
+import Footer from "./components/Footer";
 
 const App = () => {
   return (
@@ -11,6 +12,7 @@ const App = () => {
       <Hero/>
       <IdeaSection/>
       <Clients/>
+      <Footer/>
     </div>
   )
 }
---
+++ src/components/Footer.js
@@ -0,0 +1,34 @@
+import { ReactComponent as Nugs } from "../images/Nugs.svg";
+import { ReactComponent as Ig } from "../images/IG.svg";
+import { ReactComponent as Tw } from "../images/TW.svg";
+import { ReactComponent as Li } from "../images/LI.svg";
+
+
+const Footer = () => {
+    return (
+        <footer>
+            <a className="footer--lets--talk--link" href="">
+                <h4>Let's talk</h4>
+                <span>hi@rsq.com</span>
+            </a>
+            <div className="socials--locations--container">
+                <nav className="socials--container">
+                    <div className="nugs--container">
+                        <div className="plus"></div>
+                        <Nugs/>
+                    </div>
+                    <Ig/>
+                    <Tw/>
+                    <Li/>
+                </nav>
+                <div className="locations--container">
+                    <h5>Mobile, AL</h5>
+                    <h5>Chicago, IL</h5>
+                    <h5>Tulsa, OK</h5>
+                </div>
+            </div>
+        </footer>
+    )
+}
+
+export default Footer;
