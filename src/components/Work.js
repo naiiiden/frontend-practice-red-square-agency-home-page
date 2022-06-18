d09e5a96f03ade6bdf163bdf70c60d5c4437c785
naiiiden <na4ko359@gmail.com>
2022-06-18 06:57:52 +0300

    work component set min height for 375px, 425px mobile widths

--- src/components/Work.js
+++ src/components/Work.js
@@ -3,9 +3,9 @@ import Arrow from "../images/arrow.svg";
 const Work = () => {
     return (
         <div className="work--container">
-            <div className="test"></div>
+            <div className="blur--bg"></div>
             <h3>All Projects</h3>
-            <a href=""><span>Work</span><img src={Arrow} alt=""/></a>
+            <a href="https://www.redsquareagency.com/work/"><span>Work</span><img src={Arrow} alt=""/></a>
         </div>
     )
 }
