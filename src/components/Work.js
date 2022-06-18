3fafcdcad2267b923dba7f0b7d1f214c6094156e
naiiiden <na4ko359@gmail.com>
2022-06-18 06:44:06 +0300

    work component

---
+++ src/components/Work.js
@@ -0,0 +1,12 @@
+import Arrow from "../images/arrow.svg";
+
+const Work = () => {
+    return (
+        <div className="work--container">
+            <h3>All Projects</h3>
+            <a href=""><span>Work</span><img src={Arrow} alt=""/></a>
+        </div>
+    )
+}
+
+export default Work;
