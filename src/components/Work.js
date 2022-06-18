9c80ac932f000a04445d62b58fc9422dd9e16a33
naiiiden <na4ko359@gmail.com>
2022-06-18 07:16:31 +0300

    work component styled on desktop & add hover effect

--- src/components/Work.js
+++ src/components/Work.js
@@ -3,7 +3,7 @@ import Arrow from "../images/arrow.svg";
 const Work = () => {
     return (
         <div className="work--container">
-            <div className="blur--bg"></div>
+            <a href="https://www.redsquareagency.com/work/" className="blur--bg"></a>
             <h3>All Projects</h3>
             <a href="https://www.redsquareagency.com/work/"><span>Work</span><img src={Arrow} alt=""/></a>
         </div>
