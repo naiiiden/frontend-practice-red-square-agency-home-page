afef2568813732733ccded50f7a703c0ff55b670
naiiiden <na4ko359@gmail.com>
2022-10-04 16:45:22 +0300

    work.js refactor

--- src/components/Work.js
+++ src/components/Work.js
@@ -3,9 +3,9 @@ import Arrow from "../images/arrow.svg";
 const Work = () => {
     return (
         <div className="work--container">
-            <a href="https://www.redsquareagency.com/work/" className="blur--bg"></a>
+            <a href="https://www.redsquareagency.com/work/" className="blur--bg" aria-label="See our projects"></a>
             <h3>All Projects</h3>
-            <a href="https://www.redsquareagency.com/work/"><span>Work</span><img src={Arrow} alt=""/></a>
+            <span href="https://www.redsquareagency.com/work/">Work<img src={Arrow} alt=""/></span>
         </div>
     )
 }
